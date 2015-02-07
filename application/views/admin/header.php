cbc427dbf745f68d932a26346e3440d3e300ba09
Michael Weitzman <michaelweitzman@michaels-mbp-3.local.tld>
2015-02-07 13:02:11 -0800

    updating header

--- application/views/admin/header.php
+++ application/views/admin/header.php
@@ -16,9 +16,6 @@
 		    <div class="navbar-header">
 		      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
 		        <span class="sr-only">Toggle navigation</span>
-		        <span class="icon-bar"></span>
-		        <span class="icon-bar"></span>
-		        <span class="icon-bar"></span>
 		      </button>
 		      <a class="navbar-brand" href="#">Brand</a>
 		    </div>
@@ -26,18 +23,13 @@
 		    <!-- Collect the nav links, forms, and other content for toggling -->
 		    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 		      <ul class="nav navbar-nav">
-		        <li class="active"><a href="#">Orders <span class="sr-only">(current)</span></a></li>
+		        <li class="active"><a href="#">Orders<span class="sr-only">(current)</span></a></li>
 		        <li><a href="#">Products</a></li>
-
 		      </ul>
-		 
 		      <ul class="nav navbar-nav navbar-right">
-		    
 		            <li><a href="#">Logoff</a></li>
 		          </ul>
 		    </div><!-- /.navbar-collapse -->
 		  </div><!-- /.container-fluid -->
 		</nav>
 	</div>
-</body>
-</html>
